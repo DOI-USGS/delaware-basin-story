e73f810f19d8b0f2ef233ae44c5339dc62c94b9e
Bechtel <ebechtel@contractor.usgs.gov>
2020-06-10 17:15:22 -0400

    rewrote text and added intro watercolor image

--- src/views/grandChallenges/GrandChallenges.vue
+++ src/views/grandChallenges/GrandChallenges.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="grand-challenges">
     <h2>Place holder for Grand Challenges</h2>
-
+    <p>
+      Everything has an impact on one measure of water quality or another.  
+    </p>
+    <p>
+      When it rains on bare soil, the runoff carries sediment into the river and can create sediment plumes that are harmful to ____.  In the winter, runoff from melting snow can carry road salt into the river, creating a pulse of salinity that travels down the waterway. In the summer, when rainfall in scarce, a front of salty ocean water can push its way up the river closer and closer to drinking water intakes.  During the summer of 1960, the salt front pushed its way up to _____ and almost ____.
+    </p>
+    <p>
+      With the water availability for the whole river basin at stake, it’s incredibly important that the players responsible for the balance of the whole river basin make the smartest decisions they can, as fast as they can.
+    </p>
+    <p>
+      So how has the monitoring and management of such an expansive, interconnected river basin been possible?  
+    </p>
     <hr>
   </div>
 </template>
--- src/views/usesNeedsConflicts/UsesNeedsConflicts.vue
+++ src/views/usesNeedsConflicts/UsesNeedsConflicts.vue
@@ -1,30 +1,29 @@
 <template>
   <div id="uses-needs-conflicts">
     <h2>Place holder for Uses Needs Conflicts</h2>
+    <p>
+      Throughout its history, the Delaware River Basin has been a crucible of competing interests and water needs, and a clear example of the complex interplay between water quality and quantity.  Water is a finite resource, after all.
+    </p>
+    <p>
+      Balancing the just needs of the people and ecosystems in the Basin to get <em>enough</em> water has been the focus of years of research, collaboration among federal, state, and local water management groups, and even a Supreme Court decision in 1954.  That court decree gave guidelines for how much water individual players like New York City or New Jersey State is allowed to divert for its drinking water, as well as how much remaining water has to be allowed to flow at two important checkpoints along its way to the ocean.
+    </p>
     <img src="@/assets/usesNeedsConflicts/images/river-shots.png">
+
     <p>
-      In the DRB and many other places, water is a finite resource.  ​
-      [ insert something about here recognizing the perils of Tragedy of the Commons style thinking ]
+      At those two checkpoints – USGS river monitoring stations in Montague and Trenton, NJ – the minimum amount of flow has to still be able to provide enough water for agriculture, industry, ___, and recreation.  It is also set to make sure there’s enough freshwater flowing into the estuary at the mouth of the river to maintain the right mix of fresh-and-saltiness for the important ecological communities there.
     </p>
     <p>
-      The US recognized the challenge of orchestrating all the players in the DRB to use and conserve water fairly.​
-      To ensure enough water is available for the people, industries, and ecosystems who depend on it, a Supreme Court decision in 1954 established the position of the Delaware River Master.
+     The Delaware River Master – a position established by the Supreme Court ruling – relies on rain, snowmelt, runoff, and streamflow data collected by hundreds of USGS-affiliated monitoring stations through the basin to calculate how much water needs to be stored or released from the reservoirs along its tributaries to meet its flow goals.  
     </p>
     <p>
-      The River Master is responsible for compiling data from USGS water monitoring stations to inform how much water to orchestrate the reservoirs and cities in the DRB to maintain streamflow and conserve the waters throughout the year for the entire river basin. ​
-      The River Master takes into account rainfall, runoff, and water use by major users like NYC and NJ, and then decides how much water to store or release in the many reservoirs throughout the basin.
+      But alone, enough water is not enough. Water quantity and quality are deeply intertwined.  
     </p>
     <p>
-      The River Master monitors each reservoir and downstream river reach carefully.​
-      To get a rough target of success, the 1954 decree required the River Master to maintain flow at two important checkpoints along the way. ​
-      X at a Montague and Y at Trenton.
+      Humans have enormous power to alter the ecosystems they are a part of, both intentionally and unintentionally.  On top of all the competing water needs of the Basin’s cities, agriculture, ___ and ecosystems, every activity in the Basin has an impact on water quality measures as well. 
     </p>
     <p />
     <p>
-      So how does the River Master orchestrate everyone?​
-      Let’s imagine a single reservoir.  ​
-      Water is always entering the reservoir, so any extra water above the “normal” flow rate downstream is saved for later.​
-      (make a joke about how it’s the opposite of a “rainy day fund”????)
+      In particular, one of the most effective water management tools we have at our disposal is deciding how much water to store or release in reservoirs.  When rain is plentiful, reservoirs fill themselves up and release only as much water as needed to maintain the flow targets set by the Supreme Court.  If a dry spell hits the Basin, the River Master can decide to release extra water from the reservoir to fill the gap between the predicted flow and the flow targets.
     </p>
     <img src="@/assets/usesNeedsConflicts/images/normal.png">
     <p>In the late summer, when there’s less rain, the River Master can decide to release more stored water in the reservoirs to make sure the Delaware River doesn’t turn into a trickle.</p>
@@ -35,9 +34,7 @@
     </p>
     <img src="@/assets/usesNeedsConflicts/images/one.png">
     <p>
-      It becomes even more complicated when the release decisions take into account more than just water quantity.​
-      Maintaining minimum flow is one thing… but we have other water quality factors to take into account. ​
-      ….dun dun dun, segue into Grand Challenges!
+      But if the extra water is released all at once, the pulse of cool, clear reservoir water could drop the water temperature of all the river reaches downstream by several degrees. The species that live in those reaches – like mussels which like it warm, and trout which like it cool – could suddenly see their environment shift dramatically with no time to adapt.
     </p>
     <img src="@/assets/usesNeedsConflicts/images/two.png">
   </div>
