dd6ffbb98cef44b545d3ed6ef105d386ed44a836
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-06-11 11:00:30 -0500

    changes for testing of automated builds

---
+++ build.sh
@@ -0,0 +1,9 @@
+echo "this is the var $E_VUE_BUILD_MODE"
+if [ "$E_VUE_BUILD_MODE" = "test" ]
+then npm run build-test
+elif [ "$E_VUE_BUILD_MODE" = "qa" ]
+then npm run build-qa
+elif [ "$E_VUE_BUILD_MODE" = "beta" ]
+then npm run build-beta
+else npm run build
+fi
