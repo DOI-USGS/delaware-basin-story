96503c4f2aa50046710c76df03a5851c8c639020
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-06-17 18:56:08 -0500

    removed references to qa

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 echo "this is the var $E_VUE_BUILD_MODE"
 if [ "$E_VUE_BUILD_MODE" = "test" ]
 then npm run build-test
-elif [ "$E_VUE_BUILD_MODE" = "qa" ]
-then npm run build-qa
 elif [ "$E_VUE_BUILD_MODE" = "beta" ]
 then npm run build-beta
 else npm run build
