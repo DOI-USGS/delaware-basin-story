7c1d9eed6dbbed33daa08760f15409d131da759c
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-06-23 15:22:06 -0500

    remove log statement

--- src/directives/customScroll.js
+++ src/directives/customScroll.js
@@ -2,7 +2,6 @@ const customScrollObserver = new IntersectionObserver(
         (entries, customScrollObserver) => {
             entries.forEach((entry) => {
                 if(entry.isIntersecting) {
-                    console.log('ran custom scroll')
                     entry.target.classList.add('enter');
                     customScrollObserver.unobserve(entry.target);
                 }
