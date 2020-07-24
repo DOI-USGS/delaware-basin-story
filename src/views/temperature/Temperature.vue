af4e289d82339a4984dd3992080be1165c669468
coll nell <collnellphd@gmail.com>
2020-07-24 16:30:14 -0500

    clean up temperature layout

--- src/views/temperature/Temperature.vue
+++ src/views/temperature/Temperature.vue
@@ -6,23 +6,23 @@
         src="@/assets/temperature/images/waterHeader.png"
         alt="water line for the top of a stream cross-section"
       >
-      <img
-        id="tempheader"
-        src="@/assets/temperature/images/BlueLarge.png"
-        alt="a blue cloud of watercolor paint"
-      >
-      <h1 class="section-title">
-        Temperature
-      </h1>
+      <div id="label-container">
+        <img
+          id="tempheader"
+          src="@/assets/temperature/images/BlueLarge.png"
+          alt="a blue cloud of watercolor paint"
+        >
+        <h1 class="section-title">
+          Temperature
+        </h1>
+      </div>
     </div>
     <div id="content">
       <div class="row-fish">
-        <div class="left">
           <div class="temperature-text">
             <p>Water temperature is inherently linked to the health and reproduction of aquatic organisms, such as trout and mussels. In the DRB there are diverse thermal habitats that support vibrant communities of aquatic life. Some factors that modify temperature (e.g. weather/climate) are not easily controlled by DRB decisionmakers, while other factors (reservoir releases and riparian land-use) may be strategically used to maintain or modify thermal regimes. A combination of monitoring and modeling lends important information to policymakers, anglers, land resource planners, and water managers; these decisions can impact economic and ecological health.  </p>
             <p>Brook trout are the only native trout species within the DRB and prefer cold water temperatures. For brook trout, fall water temperatures are an important environmental cue to initiate the reproductive cycle. Trout can also be negatively impacted when water temperatures are too high in other seasons, as warming reduces growth rates and can make trout more susceptible to diseases. </p>
           </div>
-        </div>
         <div class="right">
           <div class="image-stack">
             <img
@@ -41,6 +41,10 @@
       <div class="row-empty">
       </div>
       <div class="row-mussels">
+        <div class="temperature-text">
+            <p>Simultaneously, the federally protected dwarf wedgemussels have their own preferences for water temperatures and river flow rates. Efforts to manage water in the modern era often require consideration of the impacts of altering flows, groundwater connectivity, and stream shading on water temperatures and aquatic life.  </p>
+            <p>The USGS has a long history of monitoring water resources in the DRB, in partnership with ### stakeholders and cooperators. A recent Federal funding initiative, the Next-Generation Water Observing System (NGWOS), has made new investments in water monitoring and information delivery for DRB stakeholders. The DRB was chosen as the pilot basin for NGWOS in 2017(?verify?) and the unique needs for monitoring were taken into account as new sensor deployments were designed, old infrastructure was upgraded, and innovations in non-contact measurement techniques were tested and improved.  </p>
+          </div>
           <svg
             id="mussels"
             xmlns="http://www.w3.org/2000/svg"
@@ -224,10 +228,6 @@
               <path d="M138 390c2-1 5 1 6 2a40 40 0 017 4 28 28 0 018 12h1c-2-5-5-10-9-13a44 44 0 00-7-4c-2-1-5-3-7-2l1 1zM143 379c0-2 3-1 4-1a114 114 0 0112 3 151 151 0 0115 5v-1a102 102 0 00-18-6l-9-2c-1 0-5-1-5 2h1zM156 372a81 81 0 0125 3v-1a71 71 0 00-25-3v1zM127 399c1-1 2 0 3 1a27 27 0 013 2 32 32 0 015 6 45 45 0 016 14h2a44 44 0 00-8-16 32 32 0 00-5-5c-2-2-5-4-7-3l1 1zM131 448c9-3 24-5 25-17h-2c-1 11-15 13-23 16v1z" />
             </g>
           </svg>
-          <div class="temperature-text">
-            <p>Simultaneously, the federally protected dwarf wedgemussels have their own preferences for water temperatures and river flow rates. Efforts to manage water in the modern era often require consideration of the impacts of altering flows, groundwater connectivity, and stream shading on water temperatures and aquatic life.  </p>
-            <p>The USGS has a long history of monitoring water resources in the DRB, in partnership with ### stakeholders and cooperators. A recent Federal funding initiative, the Next-Generation Water Observing System (NGWOS), has made new investments in water monitoring and information delivery for DRB stakeholders. The DRB was chosen as the pilot basin for NGWOS in 2017(?verify?) and the unique needs for monitoring were taken into account as new sensor deployments were designed, old infrastructure was upgraded, and innovations in non-contact measurement techniques were tested and improved.  </p>
-          </div>
       </div>
     </div>
     <hr>
@@ -254,92 +254,60 @@
 
   #header {
     position: sticky;
-    display: block;
     width: 95%;
     margin-right: auto;
     margin-left: auto;
     top: 0;
     background: white;
 
-    img {
-      display: block;
-      max-width: 100%;
-      width: 100%;
-      height: auto;
-      margin-right: auto;
-      margin-left: auto;
+    #label-container {
+
+      #tempheader{
+        width: 35%;
+        margin-left: 0;
+
+      }
+      .section-title {
+        position: absolute;
+        top: 70%;
+        margin-left: 7%;
+        color: white;
+
+      }
+    }
 
       #waterline {
         margin-top: 100px;
         background: white;
-        z-index: 3;
-      }
-    }
-
-    svg {
-      position: absolute;
-      display: inline-block;
-      height: auto;
-      width: 100%;
-      top: 0;
-      left: 0;
-    }
-    #tempheader {
-      display: inline-block;
-      width: 35%;
-      margin-left: 0;
-      margin-right: auto;
-      height: auto;
-      z-index: 2;
-    }
-    h1 {
-      display: block;
-      color: white;
-    }
+        width: 100%;
+      } 
+    
   }
 
   #content {
-    display: flex;
-    flex-direction: column;
-    height: 1000px;
-    margin-bottom: -100px;
     
     .row-fish {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
       width: 100%;
 
-      .left{
-        display: flex;
-        flex-direction: column;
-        flex-basis: 50%;
-        flex: 1;
-        padding: 2%;
-
-        .temperature-text {
-            margin: 0 auto;
-            max-width: 60rem;
-            padding: 2em;
-            margin-top: 100px;
-        }
-
+      .temperature-text {
+          margin: 0 auto;
+          max-width: 60rem;
+          padding: 2em;
+          margin-top: 5%;
       }
+
       .right {
-        display: flex;
-        flex-direction: column;
-        flex-basis: 50%;
-        flex: 1;
-        height: auto;
+        float: right;
+        width: 80%;
 
         .image-stack {
           display:grid;
-          grid-template-columns: repeat(12, 1fr);
+          grid-template-columns: 1;
           position: relative;
           margin-bottom: 0px;
 
           #sadfish {
-            grid-column: 1 / 12;
+            grid-column: 1 ;
             grid-row: 1;
             z-index: 1;
           }
@@ -359,63 +327,30 @@
             -moz-animation: shake 1s cubic-bezier(.36, .1, .2, .9) both;
             -o-animation: shake 1s cubic-bezier(.36, .1, .2, .9) both;
           }
-
         }
-
-
       }
     }
     .row-empty {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
       width: 100%;
       height: 200px;
     }
 
     .row-mussels {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
       width: 100%;
 
       #mussels {
         width: 100%;
-        margin-top: 150px;
       }
       .temperature-text {
-        position: absolute;
-        width:40%;
-        padding: 2%;
-        right: 10%;
-        margin-top: 5%;
+        margin: 0 auto;
+        max-width: 60rem;
+        padding: 2em;
 
-      }
-
-      
+      }     
     }
-
   }
 }
 
-.image-stack {
-          position:relative;
-          top: 0;
-          left: 0;
-
-          #sadfish {
-            position: relative;
-            top: 0;
-            left: 0;
-          }
-          
-          #happyfish {
-            position: absolute;
-            top: -0;
-            left: 0;
-
-          }
-}
 
 @keyframes color-change {
   0%   { opacity:0; }
