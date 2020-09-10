2d23ef970f29da1cdd047cd0bd584bc8198ce3f2
Bechtel <ebechtel@contractor.usgs.gov>
2020-09-10 10:58:25 -0400

    quality - AA edits

--- src/views/qualityChallenges/QualityChallenges.vue
+++ src/views/qualityChallenges/QualityChallenges.vue
@@ -8,18 +8,8 @@
       <p>Taken alone, managing to make sure there’s <span class="emph">enough water is not enough.</span>  Water needs to be of <span class="emph">good quality,</span> too.</p>
       <p>Water quality affects whether Delaware River water is suitable for drinking, recreation, agriculture, industry, and aquatic life. But it goes the other way too – human activities like reservoir releases, water withdrawals, wastewater discharges, land use and management, and many others affect water quality as well.</p>
       <p>Water quality in the Delaware River Basin has improved greatly since the mid-20th century. It's inspiring water quality history can be explored at the <a href="https://www.nj.gov/drbc/quality/">Delaware River Basin Commission's recap</a>, and raw data can be downloaded from the <a href="https://waterdata.usgs.gov/nwis/qw">National Water Information System</a> and <a href="https://www.waterqualitydata.us/">Water Quality Portal.</a></p>
-      <p>The USGS monitors lots of water quality measures, such as:</p>
-      <ul>
-        <li>Temperature</li>
-        <li>Salinity</li>
-        <li>Sediment and turbidity </li>
-        <li>Nutrients</li>
-        <li>Dissolved oxygen</li>
-        <li>Pathogens</li>
-        <li>Metals, including mercury </li>
-        <li>Chemical contaminants, including PCBs and PFAS</li>
-      </ul>
-      <p>In the rest of this story, we’ll dive into two water-quality variables that have drawn recent attention – salinity and temperature – and a new USGS monitoring program that is improving our ability to provide the data needed to manage both the quantity and the quality of water in the Delaware River.</p>
+      <p>The USGS and collaborators study a multitude of water-quality factors in the Basin. These include sediment, turbidity, nutrients, dissolved oxygen, pathogens, heavy metals, and chemical contaminants, each with a story of its own.</p>
+      <p>In this story, we’ll dive into two water-quality variables that have drawn recent attention – salinity and temperature – and a new USGS monitoring program that is improving our ability to provide the data needed to manage both the quantity and the quality of water in the Delaware River.</p>
     </div>
   </section>
 </template>
