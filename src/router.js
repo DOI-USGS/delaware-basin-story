f8c297d5b2386fbc7c5715d876ad5af1788b8910
ellenbechtel <ellen.bechtel@gmail.com>
2020-12-02 10:25:02 -0500

    New meta card

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Meta from 'vue-meta'
 
-Vue.use(Router);
+Vue.use(Router)
+Vue.use(Meta)
 
 export default new Router({
     routes: [
