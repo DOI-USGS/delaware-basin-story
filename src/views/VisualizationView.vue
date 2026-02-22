0140d9a1eeef86017ac19d16d6eee8f7d92ab7c8
Cee <cnell@usgs.gov>
2026-02-22 12:53:12 -0600

    use asynccomponent to render sections

--- src/views/VisualizationView.vue
+++ src/views/VisualizationView.vue
@@ -12,22 +12,29 @@
 </template>
 
 <script>
+    import { defineAsyncComponent } from 'vue';
+    import { mapState } from 'pinia';
+    import { useAppStore } from '@/stores/appStore';
+
     const appTitle = import.meta.env.VITE_APP_TITLE;
 
     export default {
         name: 'Visualization',
         components: {
-            Intro: () => import("@/components/story/intro/StoryIntro.vue"),
-            Flow: () => import("@/components/story/flow/ManagingFlow.vue"),
-            QualityChallenges: () => import("@/components/story/qualityChallenges/QualityChallenges.vue"),
-            Temperature: () => import("@/components/story/temperature/StoryTemperature.vue"),
-            Salinity: () => import("@/components/story/salinity/ManagingSalinity.vue"),
-            Monitoring: () => import("@/components/story/monitoringConclusion/StoryMonitoring.vue"),
-            Conclusion: () => import("@/components/story/conclusion/StoryConclusion.vue")
+            Intro: defineAsyncComponent(() => import("@/components/story/intro/StoryIntro.vue")),
+            Flow: defineAsyncComponent(() => import("@/components/story/flow/ManagingFlow.vue")),
+            QualityChallenges: defineAsyncComponent(() => import("@/components/story/qualityChallenges/QualityChallenges.vue")),
+            Temperature: defineAsyncComponent(() => import("@/components/story/temperature/StoryTemperature.vue")),
+            Salinity: defineAsyncComponent(() => import("@/components/story/salinity/ManagingSalinity.vue")),
+            Monitoring: defineAsyncComponent(() => import("@/components/story/monitoringConclusion/StoryMonitoring.vue")),
+            Conclusion: defineAsyncComponent(() => import("@/components/story/conclusion/StoryConclusion.vue"))
         },
         computed: {
+            ...mapState(useAppStore, {
+                introSectionRenderedFlag: 'introSectionRendered'
+            }),
             checkIfIntroSectionIsRendered() {
-                return this.$store.state.introSectionRendered;
+                return this.introSectionRenderedFlag;
             }
         },
         metaInfo: {
